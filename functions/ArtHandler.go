255a5f34f7ed1bf5e2bef512cf5979e59229af15
Badrone7 <badrguitoni4@gmail.com>
2025-12-28 22:37:18 +0100

    final result of the stylised version

--- functions/ArtHandler.go
+++ functions/ArtHandler.go
@@ -21,38 +21,69 @@ var (
 	tmpl    = template.Must(template.ParseFiles("static/index.html"))
 )
 
+func TemplatesHandler(err int, art Artstr, w http.ResponseWriter) {
+	var template *template.Template
+	switch err {
+	case 400:
+		template = tmpl400
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "text/html")
+		template.Execute(w, nil)
+	case 404:
+		template = tmpl404
+		w.WriteHeader(http.StatusNotFound)
+		w.Header().Set("Content-Type", "text/html")
+		template.Execute(w, nil)
+	case 500:
+		template = tmpl500
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "text/html")
+		template.Execute(w, nil)
+	case 599:
+		template = tmpl
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "text/html")
+		template.Execute(w, art)
+	default:
+		template = tmpl
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/html")
+		template.Execute(w, art)
+	}
+}
+
 func ArtHandler(art Artstr) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusBadRequest)
-			tmpl400.Execute(w, nil)
+			TemplatesHandler(400, art, w)
 			return
 		}
 		err := r.ParseForm()
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			tmpl400.Execute(w, nil)
+			TemplatesHandler(400, art, w)
 			return
 		}
 		art.text = r.FormValue("text")
+		if len(art.text) > 10000 {
+			art.Art = "Input text is too long."
+			TemplatesHandler(599, art, w)
+			return
+		}
 		art.style = r.FormValue("banner")
 		if !IsValidInput(art.style) {
 			art.Art = "Invalid style selected."
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusInternalServerError)
-			tmpl.Execute(w, art)
+			TemplatesHandler(599, art, w)
 			return
 		}
 		result, errart, i := ArtMaker(art.text, art.style)
 		if errart != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			tmpl500.Execute(w, nil)
+			TemplatesHandler(500, art, w)
 			return
 		} else if i == 1 {
-			w.WriteHeader(http.StatusInternalServerError)
+			TemplatesHandler(599, art, w)
+			return
 		}
-		w.Header().Set("Content-Type", "text/html")
 		art.Art = string(result)
-		tmpl.Execute(w, art)
+		TemplatesHandler(200, art, w)
 	}
 }
